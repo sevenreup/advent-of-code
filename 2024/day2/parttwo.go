6da18c259a2098d24ab696edd0fdd31e4ccc5e64
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-12-03 23:42:13 +0200

    Update parttwo.go

--- 2024/day2/parttwo.go
+++ 2024/day2/parttwo.go
@@ -28,26 +28,15 @@ func getUnsafeLevelsIndex(report []int) int {
 	if len(report) <= 1 {
 		return -1
 	}
-	flagIncrease, flagDecrease := false, false
+	isIncreasing := report[1] > report[0]
 	for i := 1; i < len(report); i++ {
 		level := report[i]
 		previousLevel := report[i-1]
-		remainder := level - previousLevel
-		diffValid := abs(remainder) >= 1 && abs(remainder) <= 3
+		isContinuing := (isIncreasing && level >= previousLevel) || (!isIncreasing && level <= previousLevel)
 
-		if remainder > 0 {
-			flagIncrease = true
-		} else if remainder < 0 {
-			flagDecrease = true
-		} else {
-			return i
-		}
-
-		if flagDecrease && flagIncrease {
-			return i
-		}
-
-		if !diffValid {
+		remainder := abs(previousLevel - level)
+		diffValid := remainder >= 1 && remainder <= 3
+		if !isContinuing || !diffValid {
 			return i
 		}
 	}
@@ -59,5 +48,4 @@ func copyNewArray(report []int, unsafeLevelIndex int) []int {
 	copy(deleted, report[:unsafeLevelIndex])
 	copy(deleted[unsafeLevelIndex:], report[unsafeLevelIndex+1:])
 	return deleted
-
 }
